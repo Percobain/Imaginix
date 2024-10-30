746c30aaf7e3bbb695f0cdca5d93ed92c81baccb
Shreyans T <shreyans.tatiya@gmail.com>
2024-10-31 02:30:42 +0530

    done and dusted

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Imaginix",
+  description:
+    "Draw Real Time AI generally denotes an artificial intelligence application capable of analyzing, synthesizing, or transforming visual content in real-time. This encompasses activities such as rendering, illustrating, or manipulating images with the proficiency of a human artist, but executed autonomously and instantaneously by the AI.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,98 @@
+
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import * as fal from "@fal-ai/serverless-client";
+
+import dynamic from "next/dynamic";
+
+const DrawingCanvas = dynamic(
+  () => import("~@/DrawingCanvas").then((mod) => mod.default),
+  {
+    ssr: false,
+  }
+);
+
+// ***** Setup the proxy
+
+// The proxy will protect your API Key and prevent it from being exposed to the client.Usually app implementation have to handle that integration themselves, but in order to make the integration as smooth as possible, we provide a drop -in proxy implementation that can be integrated with the App Router.
+
+// The proxy will also handle the request and response.
+fal.config({
+  proxyUrl: "/api/fal/proxy",
+});
+
+const seed = Math.floor(Math.random() * 100000); // Generate a random seed for numbers.
+const baseArgs = {
+  sync_mode: true,
+  strength: 0.99,
+  seed,
+};
+
+export default function Home() {
+  const [input, setInput] = useState("Anthropomorphic cat dressed as a pilot");
+  const [image, setImage] = useState(null); // Image will be updated as the Image come back from the fal API's
+
+  const [canvasImageData, setCanvasImageData] = useState("");
+
+  const { send } = fal.realtime.connect("110602490-sdxl-turbo-realtime", {
+    connectionKey: "110602490-lora-realtime",
+    onResult: (result: any) => {
+      if (result.error) return;
+
+      if (result.images && result.images[0]) {
+        setImage(result.images[0].url);
+      }
+    },
+    onError: (error: any) => {
+      console.error(error);
+    },
+  });
+
+  return (
+    <main className="flex min-h-screen flex-col p-24">
+      <p className="text-xl mb-2">FAL SDXL</p>
+      <input
+        type="text"
+        className="border border-black rounded-md p-2 w-full mb-2"
+        value={input}
+        onChange={async (e) => {
+          const { value } = e.target;
+          setInput(value);
+
+          // then send the update to the fal API
+          send({
+            ...baseArgs,
+            prompt: value,
+            image_url: canvasImageData,
+          });
+        }}
+      />
+
+      <div className="flex">
+        <DrawingCanvas
+          onCanvasChange={(canvasEvent) => {
+            const { imageData } = canvasEvent;
+            setCanvasImageData(imageData);
+
+            send({
+              ...baseArgs,
+              prompt: input,
+              image_url: imageData,
+            });
+          }}
+        />
+
+        {image && (
+          <Image
+            src={image}
+            alt="Generated Fal Image"
+            width={550}
+            height={550}
+          />
+        )}
+      </div>
+    </main>
+  );
+}
